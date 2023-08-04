5fe956f4fbbba4d933665f64ff205f66c8f3e7b6
Andrea Caggiani <caggianiandrea@gmail.com>
2023-08-04 20:46:05 +0200

    eliminazione file creati da piu di un ora

--- php/getPhoto.php
+++ php/getPhoto.php
@@ -9,7 +9,7 @@
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     try{
         $conn=new mysqli($dbAddress,$userOperator,$passOperator,$dbName);
-        $stmt=$conn->prepare("SELECT data,foto,tipo,descrizione FROM allegati WHERE incarico=?");
+        $stmt=$conn->prepare("SELECT incarico,data,foto,tipo,descrizione FROM allegati WHERE incarico=?");
         $stmt->bind_param("s",$id);
         $stmt->execute();
         $result=$stmt->get_result();
@@ -28,27 +28,41 @@
         $stmt->close();
         $conn->close();
 
+        $i=0;
         foreach($files as $k=>$v){
             $ext=explode("/",$tipo[$k]);
             $img=imagecreatefromstring($files[$k]);
-            imagepng($img,"../tmp/img".$id.".".$ext[1]);
-            if(!createImg($ext[1],$img,$id)){
+            // imagepng($img,"../tmp/img".$id.".".$ext[1]);
+            if(!createImg($ext[1],$img,$id,$i)){
                 echo json_encode(["error"=>"yes"]);
                 return;
             }
+
+            $i++;
         }
 
         echo json_encode(["error"=>"no","num"=>$i]);
+
+        //controllo file presenti da piu di un ora
+        date_default_timezone_set("Europe/Rome");
+        $filesInDir=scandir("../tmp",SCANDIR_SORT_DESCENDING);
+        unset($filesInDir[count($filesInDir)-1]);
+        unset($filesInDir[count($filesInDir)-1]);
+        foreach($filesInDir as $v){
+            $h=((time()-filemtime("../tmp/$v"))/3600);
+            if($h>0)
+                unlink("../tmp/$v");
+        }
     }catch(Exception $e){
         echo json_encode(["error"=>"yes"]);
     }
 
-    function createImg($ext,$img,$id){
+    function createImg($ext,$img,$id,$imgCount){
         switch($ext){
             case "png":
-                return imagepng($img,"../tmp/img".$id.".".$ext);
+                return imagepng($img,"../tmp/img".$id."_".$imgCount.".".$ext);
             case "jpeg":
-                return imagejpeg($img,"../tmp/img".$id.".".$ext);
+                return imagejpeg($img,"../tmp/img".$id."_".$imgCount.".".$ext);
             default:
                 return false;
         }
